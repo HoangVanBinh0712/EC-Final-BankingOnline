8feb84080c1b81566da2790d1ecd60248e9733b9
Hoàng Văn Bình <thebest11447@gmail.com>
2021-11-27 18:33:19 +0700

    make it better

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //
 const controller = require('./controllers/controller')
 const cron = require('node-cron')
-cron.schedule('0 30 * * * *', () => {
+cron.schedule('0 0 0 * * *', () => {
 	controller.tangtien()
 	controller.updategoitietkiem()
 	controller.daohan()
