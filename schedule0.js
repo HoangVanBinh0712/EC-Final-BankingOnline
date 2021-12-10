68eba60ce75af3cbedfc3b414cab27553bd846f4
Hoàng Văn Bình <thebest11447@gmail.com>
2021-12-10 16:50:54 +0700

    make it better

--- schedule0.js
+++
@@ -1,7 +0,0 @@
-const controller = require('./controllers/controller')
-const schedule = require('node-schedule');
-schedule.scheduleJob('30 * * * * *', ()=>{
-    // Execute something every 15 minutes
-    controller.tangtien()
-	console.log("work done")
-});
